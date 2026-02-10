19bf3ecf93d8901bf365e22eaa1890b93b3297bb
Aasiyah-Khan <139803670+Aasiyah-Khan@users.noreply.github.com>
2026-02-10 09:13:45 -0500

    simple aiming system
    nothing happens when "bird" is shot

--- ItStartsWithaWormm/Assets/Scripts/HumanCode/FollowMouse.cs
+++ ItStartsWithaWormm/Assets/Scripts/HumanCode/FollowMouse.cs
@@ -8,21 +8,24 @@ public class FollowMouse : MonoBehaviour
     [SerializeField]
     private float speed = 10f;
 
+    public Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         mainCamera = Camera.main;
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         followMousePos();
     }
 
     private void followMousePos()
     {
-        transform.position = GetWorldPositionFromMouse();
+        rb.MovePosition(GetWorldPositionFromMouse());
     }
 
     private Vector2 GetWorldPositionFromMouse()
